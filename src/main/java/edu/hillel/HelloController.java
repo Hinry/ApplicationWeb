5970cd29b2120b19745b3450ca513df6ccbae891
Niquesann <nipponnpon@gmail.com>
2016-11-28 21:46:06 +0200

    commit1

--- src/main/java/edu/hillel/HelloController.java
+++ src/main/java/edu/hillel/HelloController.java
@@ -1,9 +1,12 @@
 package edu.hillel;
 
+import com.sun.org.apache.xpath.internal.operations.Mod;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.context.request.WebRequest;
 
 /**
  * Created by HillelNew8 on 28.11.2016.
@@ -15,5 +18,12 @@ public class HelloController {
     public String doWelcome(ModelMap model){
         model.addAttribute("message", "Hello World!");
         return "welcome";
+    }
+    @RequestMapping(method = RequestMethod.POST)
+    public String doPostWelcome(WebRequest request, ModelMap model){
+
+        String user = request.getParameter("name");
+        model.addAttribute("user", user);
+        return "accepted";
     }
 }
--- src/main/webapp/pages/accepted.jsp
+++ src/main/webapp/pages/accepted.jsp
@@ -2,21 +2,15 @@
   Created by IntelliJ IDEA.
   User: HillelNew8
   Date: 28.11.2016
-  Time: 20:49
+  Time: 21:27
   To change this template use File | Settings | File Templates.
 --%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
 <head>
-    <title>Welcome</title>
+    <title>Title</title>
 </head>
 <body>
-
-     <h1>${message}</h1>
-
-     <form action="/welcome" method="post">
-         <label>Введите имя: </label><input name="name">
-         <button type="submit">Ok</button>
-</form>
+<h1>Доброго времени суток ${user}!</h1>
 </body>
 </html>
